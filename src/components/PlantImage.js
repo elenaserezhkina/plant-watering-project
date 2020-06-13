a7eabf3e4e5d78b6cce354592b83bf82c4732055
Elena <anel92r@gmail.com>
2020-06-13 15:05:00 +0200

    add few first plants and add function to find the plant

--- src/components/PlantImage.js
+++ src/components/PlantImage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Image } from "react-native";
 
-const PlantImage = () => (
-  <Image source={require("./img/1.jpg")} style={{ width: 200, height: 200 }} />
+const PlantImage = ({ img }) => (
+  <Image source={img} style={{ width: 200, height: 200 }} />
 );
 export default PlantImage;
--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   TouchableOpacity,
   Text,
@@ -7,29 +7,54 @@ import {
   Alert,
   StyleSheet,
 } from "react-native";
-import PlantImage from "../PlantImage";
+import PlantImage from "../components/PlantImage";
+import AllPlants from "../AllPlants";
 
 const Plant = ({ plantName }) => {
   const [waterTimes, setWaterTimes] = useState(2);
+  const [plant, setPlant] = useState(null);
+
+  useEffect(
+    function findPlant() {
+      const requestedPlant = AllPlants.find(
+        (plant) => plant.name.toLowerCase() === plantName.toLowerCase()
+      );
+      if (requestedPlant) {
+        setPlant(requestedPlant);
+      }
+    },
+    [plantName]
+  );
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.header}>{plantName}</Text>
-      </View>
-      <View>
-        <PlantImage />
-        <Button
-          onPress={() => Alert.alert("Simple Button pressed")}
-          title="Learn More"
-          color="#3BBF8F"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-      <View>
-        <Text style={styles.text}>
-          Your {plantName} should be watered {waterTimes} x week
-        </Text>
-      </View>
+    <View>
+      {plant ? (
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.header}>{plantName}</Text>
+          </View>
+          <View>
+            <PlantImage img={plant.image} />
+            <Button
+              onPress={() => Alert.alert("Simple Button pressed")}
+              title="Learn More"
+              color="#3BBF8F"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+          <View>
+            <Text style={styles.text}>
+              Your {plantName} should be watered {plant.watering}
+            </Text>
+          </View>
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.notFound}>
+            Plant not found. We dont have {plantName} yet in our base
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -42,12 +67,21 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#4f603c",
+    fontSize: 18,
+    margin: 15,
+    textAlign: "center",
   },
   header: {
     fontSize: 22,
     color: "#3BBF8F",
     textAlign: "center",
     margin: 15,
+  },
+  notFound: {
+    textAlign: "center",
+    fontSize: 20,
+    margin: 20,
+    color: "#590515",
   },
 });
 export default Plant;
